94d8f91631a92d8ef5eab81e1c529eb06fbd6678
Diego Gomes Ferreira <diegomes.ferreira.dgf@gmail.com>
2020-08-19 01:22:47 -0300

    Update Bin2Dec.py

--- Bin2Dec.py
+++ Bin2Dec.py
@@ -1,1 +1,11 @@
-print("Olá mundo")
+numero = list(input("Digite um numero binario:"))
+
+valor = 0
+
+for x in range(len(numero)):
+    print(numero, x)
+    digito = numero.pop()
+    if digito == "1":
+        valor = valor + pow(2, x)
+
+print(f"O valor decimal é {valor}")
